9560caba770286d8daf63ed5c2f302a0bd276afc
Airton <airtonbjunior@gmail.com>
2017-04-01 17:23:29 -0300

    Basic Skelleton

---
+++ computational-intelligence/genetic-algorithms/project/js/GA.js
@@ -0,0 +1,16 @@
+var AG = angular.module('AG', []);
+
+AG.controller('AGController', ['$scope', function($scope){
+
+	/* GA Parameters */
+	$scope.generations = 0;
+	$scope.population  = 0;
+	/* GA Parameters */
+
+
+
+	$scope.createPopulation = function() {
+		/* Create the population - Randomize */
+	}
+
+}]);

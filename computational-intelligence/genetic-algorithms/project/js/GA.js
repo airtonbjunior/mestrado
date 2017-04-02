580fac849434c25e1abbe4f069eb084980863d2d
Airton <airtonbjunior@gmail.com>
2017-04-02 14:07:59 -0300

    Some GA parameters

--- computational-intelligence/genetic-algorithms/project/js/GA.js
+++ computational-intelligence/genetic-algorithms/project/js/GA.js
@@ -5,12 +5,17 @@ AG.controller('AGController', ['$scope', function($scope){
 	/* GA Parameters */
 	$scope.generations = 0;
 	$scope.population  = 0;
+	$scope.mutationProability   = 0;
+	$scope.crossoverProbability = 0;
 	/* GA Parameters */
 
 
 
 	$scope.createPopulation = function() {
 		/* Create the population - Randomize */
+
 	}
+
+	
 
 }]);

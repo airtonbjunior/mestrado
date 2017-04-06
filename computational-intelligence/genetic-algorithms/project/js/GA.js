363cd99ff29761fba388170402139aa8e4147491
Airton <airtonbjunior@gmail.com>
2017-04-06 00:51:15 -0300

    Create Population and Evaluate Population Functions

--- computational-intelligence/genetic-algorithms/project/js/GA.js
+++ computational-intelligence/genetic-algorithms/project/js/GA.js
@@ -1,23 +1,83 @@
 var AG = angular.module('AG', []);
 
-AG.controller('AGController', ['$scope', function($scope){
+AG.controller('AGController', ['$scope', function($scope) {
 
 	/* GA Parameters */
 	$scope.generations          = 0;
-	$scope.populationSize       = 0;
+	$scope.populationSize       = 20;
 	$scope.mutationProability   = 0;
 	$scope.crossoverProbability = 0;
 	/* GA Parameters */
 
-	$scope.population
+
+	/* Main Variables */
+	$scope.population     = [];
+	$scope.sizeChromosome = itens.length; // Size of itens list
+	$scope.maxSizeBag = 100;
+	/* Main Variables */
 
 
 
-	$scope.createPopulation = function() {
-		/* Create the population - Randomize */
 
+
+	/* Evaluate each chromosome by the evaluate function - based on maxSizeBag - I want maximize the value here, respecting the constraints*/
+	$scope.evaluateFunction = function(chromosome) {
+		
+		var totalWeight = 0;
+		var totalValue  = 0;
+
+		for (var i = 0; i < chromosome.length; i++) {
+			if(chromosome[i] === 1) {
+				totalWeight += parseInt(itens[i].weight);  // change if I want a floor weight. I can use parselFloat();
+				totalValue  += parseInt(itens[i].value);
+			}
+		}
+
+
+		if(totalWeight > $scope.maxSizeBag) 
+			chromosome['evaluateValue'] = 0; // penalize here!
+		else
+			chromosome['evaluateValue'] = totalValue;
+		
 	}
 
+
+
+
+
+	/* Create the population randomly */
+	$scope.createPopulation = function() {
+		for (var i = 0; i < $scope.populationSize; i++) {
+			
+			$scope.population[i] = new Array();
+			
+			for (var j = 0; j < $scope.sizeChromosome; j++)
+				$scope.population[i].push(Math.round(Math.random()));
+
+		}
+	}
+
+
+
 	
+	$scope.createPopulation();
+	$scope.evaluateFunction($scope.population[0])
+
+
+	log(itens);		
+	log($scope.population);
+
+
+
+
+
+	/* Aux functions */
+	function log(info, textarea) {
+		if (!!!textarea)
+			console.log(info)
+		else { // link the textarea here }
+			
+		}
+	}
 
 }]);
---
+++ computational-intelligence/genetic-algorithms/project/js/itens.js
@@ -0,0 +1,46 @@
+itens = [
+  
+  {
+    name: "Item 1", 
+    weight: "20", 
+    unityMeasure: "Kg", 
+    value: "20", 
+    currency: "R$"
+  }, 
+  {
+    name: "Item 2", 
+    weight: "30", 
+    unityMeasure: "Kg", 
+    value: "10", 
+    currency: "R$"
+  }, 
+  {
+    name: "Item 3", 
+    weight: "10", 
+    unityMeasure: "Kg", 
+    value: "80", 
+    currency: "R$"
+  }, 
+  {
+    name: "Item 4", 
+    weight: "50", 
+    unityMeasure: "Kg", 
+    value: "50", 
+    currency: "R$"
+  }, 
+    {
+    name: "Item 5", 
+    weight: "5", 
+    unityMeasure: "Kg", 
+    value: "10", 
+    currency: "R$"
+  }, 
+    {
+    name: "Item 2", 
+    weight: "15", 
+    unityMeasure: "Kg", 
+    value: "80", 
+    currency: "R$"
+  }
+
+];

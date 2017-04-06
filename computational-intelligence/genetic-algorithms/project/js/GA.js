7cc102877fbf9e151a2a2ce0ad4aab1b147363c5
Airton <airtonbjunior@gmail.com>
2017-04-05 23:53:27 -0300

    Skeleton

--- computational-intelligence/genetic-algorithms/project/js/GA.js
+++ computational-intelligence/genetic-algorithms/project/js/GA.js
@@ -3,11 +3,13 @@ var AG = angular.module('AG', []);
 AG.controller('AGController', ['$scope', function($scope){
 
 	/* GA Parameters */
-	$scope.generations = 0;
-	$scope.population  = 0;
+	$scope.generations          = 0;
+	$scope.populationSize       = 0;
 	$scope.mutationProability   = 0;
 	$scope.crossoverProbability = 0;
 	/* GA Parameters */
+
+	$scope.population
 
 
 

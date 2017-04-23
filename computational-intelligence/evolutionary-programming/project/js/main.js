56b728afa3da59938fe0a4973fffcd94a1d76300
Airton <airtonbjunior@gmail.com>
2017-04-22 23:50:13 -0300

    Create population function, some other helpers functions

--- computational-intelligence/evolutionary-programming/project/js/main.js
+++ computational-intelligence/evolutionary-programming/project/js/main.js
@@ -10,25 +10,33 @@ UFG master's program
 
 /* Main variables */
 POPULATION_VALUES  = [];
-POPULATION_FITNESS = []
+POPULATION_FITNESS = [];
+POPULATION_SIZE    = 10;
+LOWER_LIMIT        = 1;
+UPPER_LIMIT        = 5;
 
 
+createPopulation();
 
-console.log(beale(3, 0.5));
+/* Create the population */
+function createPopulation() {
+	log("==== Creating population ====");
 
-function goldesteinPrice(x, y) {
-	//return x**y;
-	part1a = Math.pow((x + y + 1),2);
-	part1b = 19 - (14*x) + Math.pow((3*x),2) - (14*y) + (6*x*y) + Math.pow((3*y),2);
-	part1 = 1 + part1a * part1b;
+	for (var i = 0; i < POPULATION_SIZE; i++) {		
+		POPULATION_VALUES[i]  = [getRandom(LOWER_LIMIT, UPPER_LIMIT), getRandom(LOWER_LIMIT, UPPER_LIMIT )];
+		POPULATION_FITNESS[i] = 0;
+	}
 
-	part2a = Math.pow((2*x - 3*y),2);
-	part2b = 18 - (32*x) + (Math.pow((12*x),2)) + (48*y) - (36*x*y) + Math.pow((27*y),2);
-	part2 = 30 + part2a * part2b;
-
-	return part1 * part2;
-	//return (1 + (x+y+1)**2 * (19 - 14*x + (3*x)**2 - 14*y + 6*x*y + (3*y)**2)) * (30 + (2*x - 3*y)**2 * (18 - 32*x + (12*x)**2 + 48*y - 36*x*y + (27*y)**2));
+	log(POPULATION_VALUES);
 }
+
+
+
+function sortPopulationByFitness() {
+
+}
+
+
 
 
 /* Two variables functions */
@@ -41,4 +49,28 @@ function beale(x, y) {
 	part3 = Math.pow((2.625 - x + x*Math.pow(y, 3)), 2);
 
 	return part1 + part2 + part3;
+}
+
+
+function goldesteinPrice(x, y) {
+	part1a = Math.pow((x + y + 1),2);
+	part1b = 19 - (14*x) + Math.pow((3*x),2) - (14*y) + (6*x*y) + Math.pow((3*y),2);
+	part1 = 1 + part1a * part1b;
+
+	part2a = Math.pow((2*x - 3*y),2);
+	part2b = 18 - (32*x) + (Math.pow((12*x),2)) + (48*y) - (36*x*y) + Math.pow((27*y),2);
+	part2 = 30 + part2a * part2b;
+
+	return part1 * part2;
+}
+
+console.log(beale(3, 0.5));
+
+
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function log(msg, input) {
+	console.log(msg);
 }

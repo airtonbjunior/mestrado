6df4811b2a215f95c202249b923046bd8067705e
Airton <airtonbjunior@gmail.com>
2017-05-07 13:52:54 -0300

    Mutation of operator

--- computational-intelligence/genetic-programming/project/js/main.js
+++ computational-intelligence/genetic-programming/project/js/main.js
@@ -18,6 +18,14 @@ HEIGHT_TREE = 3; /* 2^h terminals and 2^h-1 operators */
 GENERATIONS = 10;
 POP_SIZE    = 30;
 POPULATION  = [];
+
+REPRODUTION_PROBABILITY = 0;
+CROSSOVER_PROBABILITY = 75;
+MUTATE_PROBABILITY = 5;
+PERMUTATION_PROBABILITY = 0;
+
+ELITISM = true;
+DESTRUCTION_TREE = false;
 /* PROBLEM PARAMETERS */
 
 
@@ -29,8 +37,12 @@ POPULATION  = [];
 
 
 /* It's a toy problem = I want to find the function x^2 + 1 */
-/**/
+
+/* 	Generate the population 
+	Default: Full Method (perfect trees)
+*/
 function generatePopulation() {
+	log("###### Generate the population ######");
 	var node = {};
 	//var operators = [];
 	//var terminals = [];
@@ -91,7 +103,11 @@ function generatePopulation() {
 }
 
 
+/*	Evaluate the expression 
+	Use the Mean Square Error
 
+	Sum([square of differences between estimator and estimated])/n
+*/
 function evaluate(expression) {
 	var exp;
 
@@ -114,6 +130,54 @@ function evaluate(expression) {
 	return parseFloat(squareError/test_values.length); // return the mean square error
 }
 
+
+
+function nextGeneration() {
+	if(getRandom(0, 100) <= CROSSOVER_PROBABILITY) {
+		//doCrossover(POPULATION[getRandom(0, POP_SIZE - 1)], POPULATION[getRandom(0, POP_SIZE - 1)]);
+
+		doMutation(POPULATION[getRandom(0, POP_SIZE - 1)]);
+	}
+}
+
+
+function doCrossover(node1, node2) {
+	log("Crossover between " + node1.expression + " and " + node2.expression);
+	do {
+		crosspoint = getRandom(0, node1.expression.length - 1);
+	} while(node1.expression.charAt(crosspoint) === "(" || node1.expression.charAt(crosspoint) === ")");
+
+	log(node1.expression.charAt(crosspoint) + " at position " + crosspoint);
+}
+
+
+function doMutation(node) {
+	log("Mutate the expression " + node.expression);
+
+	do {
+		mutatePoint = getRandom(0, node.expression.length - 1);
+	} while(node.expression.charAt(mutatePoint) === "(" || node.expression.charAt(mutatePoint) === ")");
+
+
+	/* Verify if it's a operator */
+	if(OPERATORS.indexOf(node.expression.charAt(mutatePoint)) != -1) {
+
+		log("I'll change the operator " + node.expression.charAt(mutatePoint) + " at position " + mutatePoint);
+		do {
+			newOperator = OPERATORS[getRandom(0, OPERATORS.length - 1)];
+		} while (newOperator === node.expression.charAt(mutatePoint));
+	
+		node.expression = node.expression.substring(0, mutatePoint) + newOperator + node.expression.substring(mutatePoint+1);
+	}
+	else {
+
+	}
+
+
+	log("The result is " + node.expression);
+}
+
+
 /* Sort the population by fitness */
 function sortComparator(a, b) {
 	return parseFloat(a.fitness) - parseFloat(b.fitness);
@@ -126,7 +190,7 @@ generatePopulation();
 POPULATION.sort(sortComparator);
 
 for (var i = 0; i < GENERATIONS; i++) {
-	
+	nextGeneration();
 }
 
 console.log(POPULATION);
@@ -141,6 +205,10 @@ function getRandom(min, max) {
 	return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+/* Soon will log on page */
+function log(msg, input) {
+	console.log(msg)
+}
 
 /*
 function mountExpression() {

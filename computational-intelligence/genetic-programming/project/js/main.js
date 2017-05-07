ec8eecabe0c26c86d49f369678bda7891e4b3540
Airton <airtonbjunior@gmail.com>
2017-05-06 22:33:50 -0300

    mount the expression

--- computational-intelligence/genetic-programming/project/js/main.js
+++ computational-intelligence/genetic-programming/project/js/main.js
@@ -11,9 +11,12 @@ UFG master's program
 
 /* PROBLEM PARAMETERS */
 OPERATORS = ['+', '-', '*', '/'];
-VARIABLES = ["x"];
+TERMINALS = ["x"];
 
-HEIGHT_TREE = 2; /* 2^h operators and 2^h-1 constants */
+HEIGHT_TREE = 3; /* 2^h operators and 2^h-1 constants */
+
+GENERATIONS = 10;
+POPULATION = [];
 /* PROBLEM PARAMETERS */
 
 
@@ -21,6 +24,12 @@ HEIGHT_TREE = 2; /* 2^h operators and 2^h-1 constants */
 /* It's a toy problem = I want to find the function x^2 + 1 */
 /**/
 function generatePopulation() {
+	var node = {};
+	var operators = [];
+	var terminals = [];
+
+	var expression = "";
+
 	var n_operators = parseInt(Math.pow(2, HEIGHT_TREE) - 1);
 	var n_terminals = parseInt(Math.pow(2, HEIGHT_TREE));
 
@@ -30,8 +39,81 @@ function generatePopulation() {
 	The children->right is the (2*i + 2) position 
 	*/
 
-	console.log("Number of operators: " + n_operators);
-	console.log("Number of terminals: " + n_terminals);
+	for(var i = 0; i < n_operators/2; i++) {
+		expression += "("; 
+
+		if(getRandom(0, 1) == 0) 
+			expression += TERMINALS[getRandom(0, TERMINALS.length -1)]
+		else
+			expression += getRandom(0, 9);
+
+		expression += OPERATORS[getRandom(0, OPERATORS.length - 1)];
+		
+		// Two times (didact reasons)
+		if(getRandom(0, 1) == 0) 
+			expression += TERMINALS[getRandom(0, TERMINALS.length -1)]
+		else
+			expression += getRandom(0, 9);		
+		
+		expression += ")";
+		expression += OPERATORS[getRandom(0, OPERATORS.length - 1)];
+	}
+	expression = expression.substring(0, expression.length - 1); // Remove the last operator
+
+	/*
+	for (var i = 0; i < n_operators; i++) {
+		operators.push(OPERATORS[getRandom(0, OPERATORS.length -1)]);
+	}
+
+	for (var i = 0; i < n_terminals; i++) {
+		// choose between variables and constants (50%) 
+		if(getRandom(0, 1) == 0) 
+			terminals.push(TERMINALS[getRandom(0, TERMINALS.length - 1)]);
+		else
+			terminals.push(getRandom(0, 9));		
+	}
+	*/
+
+	node.operators = operators;
+	node.terminals = terminals;
+	node.expression = expression;
+	node.fitness = 0;
+
+	POPULATION.push(node);
 }
 
-generatePopulation();
+function mountExpression() {
+	var exp = "";
+	var node = POPULATION[0];
+
+	var operatorsIndex = node.operators.length - 1;
+
+	for (var i = node.terminals.length - 1; i >= 0; i--) {
+		
+		exp += node.terminals[i];
+		if(operatorsIndex >= 0) {
+			exp += node.operators[operatorsIndex];
+			operatorsIndex--;			
+		}
+	}
+
+	console.log(exp);
+}
+
+
+for (var i = 0; i < GENERATIONS; i++) {
+	generatePopulation();
+}
+
+mountExpression();
+console.log(POPULATION);
+
+
+
+
+/* Aux functions */
+
+/* Get a random INTEGER between min and max */
+function getRandom(min, max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
+}

6c96ec1d107dee94bb1db7c4da5c5ca65ff9fb59
Airton <airtonbjunior@gmail.com>
2017-05-07 11:16:16 -0300

    evaluate function

--- computational-intelligence/genetic-programming/project/js/main.js
+++ computational-intelligence/genetic-programming/project/js/main.js
@@ -13,53 +13,64 @@ UFG master's program
 OPERATORS = ['+', '-', '*', '/'];
 TERMINALS = ["x"];
 
-HEIGHT_TREE = 3; /* 2^h operators and 2^h-1 constants */
+HEIGHT_TREE = 3; /* 2^h terminals and 2^h-1 operators */
 
 GENERATIONS = 10;
-POPULATION = [];
+POP_SIZE    = 30;
+POPULATION  = [];
 /* PROBLEM PARAMETERS */
 
+
+/* 
+	I'll represent the tree in a array 
+	- The children->left is the (2*i + 1) position 
+	- The children->right is the (2*i + 2) position 
+*/
 
 
 /* It's a toy problem = I want to find the function x^2 + 1 */
 /**/
 function generatePopulation() {
 	var node = {};
-	var operators = [];
-	var terminals = [];
+	//var operators = [];
+	//var terminals = [];
 
-	var expression = "";
+	var expression;
 
 	var n_operators = parseInt(Math.pow(2, HEIGHT_TREE) - 1);
 	var n_terminals = parseInt(Math.pow(2, HEIGHT_TREE));
 
-	/* 
-	I'll represent the tree in a array 
-	The children->left is the (2*i + 1) position 
-	The children->right is the (2*i + 2) position 
-	*/
-
-	for(var i = 0; i < n_operators/2; i++) {
-		expression += "("; 
-
-		if(getRandom(0, 1) == 0) 
-			expression += TERMINALS[getRandom(0, TERMINALS.length -1)]
-		else
-			expression += getRandom(0, 9);
-
-		expression += OPERATORS[getRandom(0, OPERATORS.length - 1)];
+	for(var j = 0; j < POP_SIZE; j++) {
 		
-		// Two times (didact reasons)
-		if(getRandom(0, 1) == 0) 
-			expression += TERMINALS[getRandom(0, TERMINALS.length -1)]
-		else
-			expression += getRandom(0, 9);		
-		
-		expression += ")";
-		expression += OPERATORS[getRandom(0, OPERATORS.length - 1)];
+		expression = "";
+		node = {};
+
+		/* Mount the expression */
+		for(var i = 0; i < n_operators/2; i++) {
+			expression += "("; 
+
+			if(getRandom(0, 1) == 0) 
+				expression += TERMINALS[getRandom(0, TERMINALS.length -1)]
+			else
+				expression += getRandom(0, 9);
+
+			expression += OPERATORS[getRandom(0, OPERATORS.length - 1)];
+			
+			// Two times (didact reasons)
+			if(getRandom(0, 1) == 0) 
+				expression += TERMINALS[getRandom(0, TERMINALS.length -1)]
+			else
+				expression += getRandom(0, 9);		
+			
+			expression += ")";
+			expression += OPERATORS[getRandom(0, OPERATORS.length - 1)];
+		}
+		expression = expression.substring(0, expression.length - 1); // Remove the last operator
+		node.expression = expression;
+		node.fitness = evaluate(expression);
+
+		POPULATION.push(node);
 	}
-	expression = expression.substring(0, expression.length - 1); // Remove the last operator
-
 	/*
 	for (var i = 0; i < n_operators; i++) {
 		operators.push(OPERATORS[getRandom(0, OPERATORS.length -1)]);
@@ -74,26 +85,22 @@ function generatePopulation() {
 	}
 	*/
 
-	node.operators = operators;
-	node.terminals = terminals;
-	node.expression = expression;
-	node.fitness = 0;
+	//node.operators = operators;
+	//node.terminals = terminals;
 
-	POPULATION.push(node);
 }
 
 
 
-function evaluate(node) {
-	var expression = node.expression;
+function evaluate(expression) {
 	var exp;
 
-	/* Tests for a certain function x^2+1*/
+	/* Tests for a certain function x^2+1 (toy problem for now) */
 	var test_values = ["1", "2", "3", "4", "5"]
 	var result_test = ["2", "5", "10", "17", "26"]
 	var differences = [];
+	/* Tests for a certain function x^2+1*/
 
-	/* Test cases */
 	for (var i = 0; i < test_values.length; i++) {
 		exp = expression.replace(/x/g, test_values[i]);	
 		console.log(parseFloat(eval(exp)) + " - " + parseFloat(result_test[i]) + " = " + parseFloat(eval(exp) - parseFloat(result_test[i])));
@@ -104,12 +111,38 @@ function evaluate(node) {
 	for (var i = 0; i < differences.length; i++) {
 		squareError += Math.pow(parseFloat(differences[i]), 2);
 	}
-	return (squareError/test_values.length); // return the mean square error
+	return parseFloat(squareError/test_values.length); // return the mean square error
 }
 
 
+function evaluateAll() {
+	for (var i = 0; i < POP_SIZE; i++) {
+
+	}
+}
 
 
+/* Main flow of Genetic Programming */
+generatePopulation();
+
+for (var i = 0; i < GENERATIONS; i++) {
+	
+}
+
+console.log(POPULATION);
+
+
+
+
+/* Aux functions */
+
+/* Get a random INTEGER between min and max */
+function getRandom(min, max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+
+/*
 function mountExpression() {
 	var exp = "";
 	var node = POPULATION[0];
@@ -127,21 +160,4 @@ function mountExpression() {
 
 	console.log(exp);
 }
-
-
-for (var i = 0; i < GENERATIONS; i++) {
-	generatePopulation();
-}
-
-evaluate(POPULATION[0]);
-console.log(POPULATION);
-
-
-
-
-/* Aux functions */
-
-/* Get a random INTEGER between min and max */
-function getRandom(min, max) {
-	return Math.floor(Math.random()*(max-min+1)+min);
-}
+*/

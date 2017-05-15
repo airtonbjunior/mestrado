3191d2f70ac2871b2fbaf33eb99eb8718cc59498
Airton <airtonbjunior@gmail.com>
2017-05-14 22:57:44 -0300

    Some test code of PRAW

--- sentiment-analysis/projects/sandbox-python/main.py
+++ sentiment-analysis/projects/sandbox-python/main.py
@@ -1,1 +1,13 @@
-print(2+2)
+import praw
+
+# User/Pass optional if access only public content 
+reddit = praw.Reddit(user_agent='Comment Extraction (by /u/sentiment_analysis)',
+                     client_id='404ucZurD8NCyg', 
+                     client_secret="HUNj14gPjK7Ua5rzDBqUIV5-CVo") 
+
+#testing some post of r/cancer/ subreddit
+submission = reddit.submission(url='https://www.reddit.com/r/cancer/comments/5bjnpi/best_chemo_buddy/')
+
+submission.comments.replace_more(limit=0)
+for comment in submission.comments.list():
+    print(comment.body)

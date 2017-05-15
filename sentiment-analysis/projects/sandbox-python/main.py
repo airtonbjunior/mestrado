4322b12394a9041adcc48e76589105d34bda6798
Airton <airtonbjunior@gmail.com>
2017-05-15 00:44:20 -0300

    Pass MoreComments problem, log the script time

--- sentiment-analysis/projects/sandbox-python/main.py
+++ sentiment-analysis/projects/sandbox-python/main.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import praw
 import io
+import time
 from praw.models import MoreComments
 
+
+start = time.time()
 
 # User/Pass optional if access only public content 
 reddit = praw.Reddit(user_agent='Comment Extraction (by /u/sentiment_analysis)',
@@ -12,14 +15,19 @@ reddit = praw.Reddit(user_agent='Comment Extraction (by /u/sentiment_analysis)',
 # Enconding problem solved on [1]
 file = io.open("D:/redditdata-cancer.txt", "w", encoding='utf-8') 
 
-for submissions in reddit.subreddit('cancer').top('all'):
+for submissions in reddit.subreddit('cancer').hot(limit=100000000):
     # submissions parameter has the id of the topic
     for comment in reddit.submission(id=submissions).comments.list():
-    	#print(comment.body.encode('utf-8'))
+    	# [2] to solve MoreComents problem
+    	if isinstance(comment, MoreComments):
+        	continue
     	file.write(comment.body) 
 
+end = time.time()
 
+print("Script ends after " + str(format(end - start, '.3g')) + " seconds")
 
 
 # Coments/References
 #[1] https://www.reddit.com/r/redditdev/comments/4vsksc/prawtheard_scraper_stops_iterating_midway/
+#[2] http://praw.readthedocs.io/en/latest/tutorials/comments.html?highlight=has%20no%20attribute%20body

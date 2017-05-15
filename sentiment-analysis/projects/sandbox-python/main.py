d415f79057aad0e658a50721adb20c553485049a
Airton <airtonbjunior@gmail.com>
2017-05-15 00:07:13 -0300

    Saving the comments of a subreddit on a file (os archive system - hardcoded here

--- sentiment-analysis/projects/sandbox-python/main.py
+++ sentiment-analysis/projects/sandbox-python/main.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 import praw
+import io
 from praw.models import MoreComments
 
 
@@ -7,10 +9,17 @@ reddit = praw.Reddit(user_agent='Comment Extraction (by /u/sentiment_analysis)',
                      client_id='404ucZurD8NCyg', 
                      client_secret="HUNj14gPjK7Ua5rzDBqUIV5-CVo") 
 
-#testing some post of r/cancer/ subreddit
-submission = reddit.submission(id='5bjnpi')
+# Enconding problem solved on [1]
+file = io.open("D:/redditdata-cancer.txt", "w", encoding='utf-8') 
+
+for submissions in reddit.subreddit('cancer').top('all'):
+    # submissions parameter has the id of the topic
+    for comment in reddit.submission(id=submissions).comments.list():
+    	#print(comment.body.encode('utf-8'))
+    	file.write(comment.body) 
 
 
-submission.comments.replace_more(limit=0)
-for comment in submission.comments.list():
-    print(comment.body)
+
+
+# Coments/References
+#[1] https://www.reddit.com/r/redditdev/comments/4vsksc/prawtheard_scraper_stops_iterating_midway/

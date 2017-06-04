eedf7567c65e4d8acf3b0045481a0fd3862850d1
Airton <airtonbjunior@gmail.com>
2017-06-04 19:30:05 -0300

    Neural Network - first steps

--- computational-intelligence/neural-networks/project/js/main.js
+++ computational-intelligence/neural-networks/project/js/main.js
@@ -8,6 +8,12 @@ Computational Intelligence - Federal University of Goias (UFG)
 UFG master's program 
 */
 
+KNOWLEDGE_RATE = 0;
+LAYERS = 0;
+INPUTS = 0;
+OUTPUTS = 0;
+
+ACTIVATION_FUNCTION = 0;
 
 /* Process before call the main start */
 function startPreparation() {

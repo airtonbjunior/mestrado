d5cb9a9bd498f74504cf60b2f113a68934c5ae24
Airton <airtonbjunior@gmail.com>
2017-06-05 02:25:32 -0300

    Create hidden layer

--- computational-intelligence/neural-networks/project/js/main.js
+++ computational-intelligence/neural-networks/project/js/main.js
@@ -11,23 +11,24 @@ UFG master's program
 LEARN_RATE = 0;
 ITERATIONS = 0;
 HIDDEN_LAYERS = 1;
-HIDDEN_LAYER = [];
-
+HIDDEN_LAYER = []; // if necessary have more than one layer, the HIDDEN_LAYER is an array
+INPUTS = 2;
+OUTPUTS = 1;
 
 PERCEPTRON = {
 	inputs: [],
 	weights: [],
-	total: 0
+	transfer_function_value: 0
 }
 
 
 INPUT_TEST = [
 	{
-		input: [0, 0], 
+		input: [1, 1], 
 		output: 0
 	}, 
 	{
-		input: [1, 1], 
+		input: [0, 0], 
 		output: 0
 	},
 	{
@@ -43,25 +44,45 @@ INPUT_TEST = [
 ACTIVATION_FUNCTION = "default";
 
 
-create_hidden_layer(3, 2);
+create_hidden_layer(3, INPUTS);
+training_network();
 console.log(HIDDEN_LAYER);
 
 
+function training_network () {
+	/* hardcoded because I'm always using 1 hidden layer. Change this to do this dinamically (loop through HIDDEN_LAYER) */
+	for (var i = 0; i < HIDDEN_LAYER[0].length; i++) {
+		set_inputs(HIDDEN_LAYER[0][i], INPUT_TEST[0].input);
+
+		/* Calc the transfer function */
+		HIDDEN_LAYER[0][i].transfer_function_value = transfer_function(HIDDEN_LAYER[0][i]);
+	}	
+}
+
+
+/* Create a hidden layer */
 function create_hidden_layer(num_perceptrons, num_weights) {
 	var layer = [];
 	var perceptron;
-	for(var i = 0; i < num_perceptrons; i++) {
-		perceptron = new Object(PERCEPTRON)
 
-		// Initialize the weights randomly [0,1]
+	for(var i = 0; i < num_perceptrons; i++) {
+		
+		perceptron = {
+			inputs: [], 
+			weights: [], 
+			transfer_function_value: 0
+		}
+
 		initialize_weights(perceptron, num_weights);
 		layer.push(perceptron);
+
+		perceptron = {};
 	}
 	HIDDEN_LAYER.push(layer);	
 }
 
 
-/* Initialize the weights randomly */
+/* Initialize the weights randomly [0,1] */
 function initialize_weights(perceptron, quantity) {
 	for(var i = 0; i < quantity; i++) {
 		perceptron.weights[i] = getRandom(0, 1);
@@ -69,11 +90,17 @@ function initialize_weights(perceptron, quantity) {
 }
 
 
+/* Set the input values to the perceptron */
+function set_inputs(perceptron, inputs) {
+	perceptron.inputs = inputs;
+}
+
+
 /* Sum of inputs * weights */
 function transfer_function(perceptron) {
 	var sum = 0;
 	for(var i = 0; i < perceptron.inputs.length; i++) {
-		sum += inputs[i] * weights[i];
+		sum += perceptron.inputs[i] * perceptron.weights[i];
 	}
 
 	return sum;
@@ -88,10 +115,6 @@ function startPreparation() {
 
 function start() {
 	
-}
-
-function training() {
-
 }
 
 

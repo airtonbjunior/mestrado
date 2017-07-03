5c1adf7d6c05cf782e7079a9638879285cb34935
Airton <airtonbjunior@gmail.com>
2017-07-02 21:28:22 -0300

    Final project updated

--- sentiment-analysis/projects/genetic-programming/symbolic-regression.py
+++ sentiment-analysis/projects/genetic-programming/symbolic-regression.py
@@ -74,7 +74,10 @@ def saveTweetsFromIdInFile():
             try:
                 tweet = twitter.show_status(id=str(tweet_parsed[0]))
                 file.write(tweet_parsed[1].strip() + "#@#" + tweet['text'].strip() + "\n") 
-            except:
+            except Exception as ex:
+                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                message = template.format(type(ex).__name__, ex.args)
+                print(message)
                 exceptions += 1
                 continue
 

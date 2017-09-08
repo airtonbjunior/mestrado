5b5a824203a609180bc26743f36b9b14884850a5
Airton <airtonbjunior@gmail.com>
2017-09-08 20:30:56 -0300

    Tabu search skelleton

---
+++ metaheuristics/tabu-search/project/js/main.js
@@ -0,0 +1,18 @@
+/* 
+Airton Bordin Junior
+airtonbjunior@gmail.com
+
+
+Tabu Search
+
+Metaheuristics - Federal University of Goias (UFG)
+UFG master's program 
+*/
+TABU_LIST_SIZE = 10
+TABU_LIST = []
+
+
+/* Move to the next element in search space */
+function move() {
+
+}

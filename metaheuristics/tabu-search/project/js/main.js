29b9f2b02de5efd1701f39b6645a18c6a82e4469
Airton <airtonbjunior@gmail.com>
2017-09-09 15:49:19 -0300

    getWeight function

--- metaheuristics/tabu-search/project/js/main.js
+++ metaheuristics/tabu-search/project/js/main.js
@@ -16,8 +16,9 @@ BEST_KNOW_SOLUTION = "";
 INITIAL_SOLUTION = [] // only for log and comparsion
 ACTUAL_SOLUTION = []
 
-BAG_CAPACITY = 300;
+BAG_CAPACITY = 150;
 ITENS_QUANTITY = itens.length;
+
 
 /* Choose a start solution. Default: randomly */
 function initializeSolution() {
@@ -30,8 +31,8 @@ function initializeSolution() {
 }
 
 
-/* Evaluate the solution value */
-function evaluateSolution(solution) {
+/* Value of the solution */
+function valueSolution(solution) {
 	sum = 0;
 	for (var i = 0; i < solution.length; i++) {
 		if(solution[i] == 1) {
@@ -41,21 +42,39 @@ function evaluateSolution(solution) {
 	return sum;
 }
 
+/* Weight of the solution */
+function weightSolution(solution) {
+	sum = 0;
+	for (var i = 0; i < solution.length; i++) {
+		if(solution[i] == 1) {
+			sum += parseInt(itens[i].weight);
+		}
+	}
+	return sum;
+}
+
+/* Get the neighbors of a solution */
 function getNeighbors(actual_solution) {
 	var neighbors = []
+	var neighbor = {}
 	var solution = actual_solution.join().split(",")
 	
 	for (var i = 0; i < solution.length; i++) {
 		if (solution[i] == "0") { solution[i] = "1" } else { solution[i] = "0" }
 		
-		neighbors.push(solution)
+		neighbor.solution = solution;
+		neighbor.value = valueSolution(solution)
+		neighbor.weight = weightSolution(solution)
+
+		neighbors.push(neighbor)
+
+		/* restart variables */
 		solution = actual_solution.join().split(",")
+		neighbor = {}
 	}
 	return neighbors;
 }
 
 initializeSolution();
 
-console.log(INITIAL_SOLUTION);
-console.log(evaluateSolution(INITIAL_SOLUTION))
 console.log(getNeighbors(INITIAL_SOLUTION))

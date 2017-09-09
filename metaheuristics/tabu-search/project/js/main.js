b05e6b3f60ed51aa882cf021b1f0eba069544e4d
Airton <airtonbjunior@gmail.com>
2017-09-09 15:20:38 -0300

    getNeighbors function

--- metaheuristics/tabu-search/project/js/main.js
+++ metaheuristics/tabu-search/project/js/main.js
@@ -8,6 +8,7 @@ Tabu Search
 Metaheuristics - Federal University of Goias (UFG)
 UFG master's program 
 */
+
 TABU_LIST_SIZE = 10;
 TABU_LIST = [];
 BEST_KNOW_SOLUTION = "";
@@ -20,6 +21,7 @@ ITENS_QUANTITY = itens.length;
 
 /* Choose a start solution. Default: randomly */
 function initializeSolution() {
+	/* For has much better performance than forEach */
 	for(var i = 0; i < ITENS_QUANTITY; i++) {
 		INITIAL_SOLUTION.push(Math.round(Math.random()));
 	}
@@ -31,7 +33,6 @@ function initializeSolution() {
 /* Evaluate the solution value */
 function evaluateSolution(solution) {
 	sum = 0;
-	/* For has much better performance than forEach */
 	for (var i = 0; i < solution.length; i++) {
 		if(solution[i] == 1) {
 			sum += parseInt(itens[i].value);
@@ -41,16 +42,20 @@ function evaluateSolution(solution) {
 }
 
 function getNeighbors(actual_solution) {
-
+	var neighbors = []
+	var solution = actual_solution.join().split(",")
+	
+	for (var i = 0; i < solution.length; i++) {
+		if (solution[i] == "0") { solution[i] = "1" } else { solution[i] = "0" }
+		
+		neighbors.push(solution)
+		solution = actual_solution.join().split(",")
+	}
+	return neighbors;
 }
-
-
-/* Move to the next element in search space */
-function move() {
-
-}
-
 
 initializeSolution();
+
 console.log(INITIAL_SOLUTION);
 console.log(evaluateSolution(INITIAL_SOLUTION))
+console.log(getNeighbors(INITIAL_SOLUTION))

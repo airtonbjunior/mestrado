022e5ec7eac55c6ae793367fd405928fc90819bc
Airton <airtonbjunior@gmail.com>
2017-09-09 13:57:03 -0300

    new functions and variables - binaryBag

---
+++ metaheuristics/tabu-search/project/js/itens.js
@@ -0,0 +1,94 @@
+itens = [
+  
+  {
+    name: "Item 1", 
+    weight: "20", 
+    unityMeasure: "Kg", 
+    value: "20", 
+    currency: "R$"
+  }, 
+  {
+    name: "Item 2", 
+    weight: "30", 
+    unityMeasure: "Kg", 
+    value: "10", 
+    currency: "R$"
+  }, 
+  {
+    name: "Item 3", 
+    weight: "10", 
+    unityMeasure: "Kg", 
+    value: "80", 
+    currency: "R$"
+  }, 
+  {
+    name: "Item 4", 
+    weight: "50", 
+    unityMeasure: "Kg", 
+    value: "50", 
+    currency: "R$"
+  }, 
+  {
+    name: "Item 5", 
+    weight: "5", 
+    unityMeasure: "Kg", 
+    value: "10", 
+    currency: "R$"
+  }, 
+  {
+    name: "Item 6", 
+    weight: "15", 
+    unityMeasure: "Kg", 
+    value: "80", 
+    currency: "R$"
+  },
+  {
+    name: "Item 7", 
+    weight: "35", 
+    unityMeasure: "Kg", 
+    value: "75", 
+    currency: "R$"
+  },
+  {
+    name: "Item 8", 
+    weight: "5", 
+    unityMeasure: "Kg", 
+    value: "10", 
+    currency: "R$"
+  },
+  {
+    name: "Item 9", 
+    weight: "40", 
+    unityMeasure: "Kg", 
+    value: "130", 
+    currency: "R$"
+  },
+  {
+    name: "Item 10", 
+    weight: "90", 
+    unityMeasure: "Kg", 
+    value: "90", 
+    currency: "R$"
+  }, 
+  {
+    name: "Item 11", 
+    weight: "30", 
+    unityMeasure: "Kg", 
+    value: "110", 
+    currency: "R$"
+  },  
+  {
+    name: "Item 12", 
+    weight: "65", 
+    unityMeasure: "Kg", 
+    value: "10", 
+    currency: "R$"
+  },  
+  {
+    name: "Item 13", 
+    weight: "50", 
+    unityMeasure: "Kg", 
+    value: "150", 
+    currency: "R$"
+  }  
+];
--- metaheuristics/tabu-search/project/js/main.js
+++ metaheuristics/tabu-search/project/js/main.js
@@ -8,11 +8,49 @@ Tabu Search
 Metaheuristics - Federal University of Goias (UFG)
 UFG master's program 
 */
-TABU_LIST_SIZE = 10
-TABU_LIST = []
+TABU_LIST_SIZE = 10;
+TABU_LIST = [];
+BEST_KNOW_SOLUTION = "";
+
+INITIAL_SOLUTION = [] // only for log and comparsion
+ACTUAL_SOLUTION = []
+
+BAG_CAPACITY = 300;
+ITENS_QUANTITY = itens.length;
+
+/* Choose a start solution. Default: randomly */
+function initializeSolution() {
+	for(var i = 0; i < ITENS_QUANTITY; i++) {
+		INITIAL_SOLUTION.push(Math.round(Math.random()));
+	}
+	ACTUAL_SOLUTION = INITIAL_SOLUTION;
+	console.log("[START INITIAL SOLUTION]: " + INITIAL_SOLUTION)
+}
+
+
+/* Evaluate the solution value */
+function evaluateSolution(solution) {
+	sum = 0;
+	/* For has much better performance than forEach */
+	for (var i = 0; i < solution.length; i++) {
+		if(solution[i] == 1) {
+			sum += parseInt(itens[i].value);
+		}
+	}
+	return sum;
+}
+
+function getNeighbors(actual_solution) {
+
+}
 
 
 /* Move to the next element in search space */
 function move() {
 
 }
+
+
+initializeSolution();
+console.log(INITIAL_SOLUTION);
+console.log(evaluateSolution(INITIAL_SOLUTION))
